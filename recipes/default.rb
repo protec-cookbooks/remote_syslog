d89734f81b94ad93eed111cd314c578e68c36f6c
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2014-02-05 16:34:02 +0000

    Install build deps for gem package, delay restart of service

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,25 @@
+pkgs = [ 'build-essential', 'libssl-dev', 'ruby-dev', 'rubygems' ]
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
 gem_package 'remote_syslog' do
     action :install
+end
+
+cookbook_file '/etc/init.d/remote_syslog' do
+    source 'remote_syslog'
+    owner 'root'
+    group 'root'
+    mode 00744
+end
+
+service 'remote_syslog' do
+    provider Chef::Provider::Service::Init::Debian
+    supports :status => true, :start => true, :stop => true, :restart => true
+    action [ :enable, :start ]
 end
 
 template '/etc/log_files.yml' do
@@ -11,18 +31,5 @@ template '/etc/log_files.yml' do
       :logs => node['remote_syslog']['log_files']
     )
     action :create
-end
-
-cookbook_file '/etc/init.d/remote_syslog' do
-	source 'remote_syslog'
-	owner 'root'
-	group 'root'
-    mode 00744
-end
-
-service 'remote_syslog' do
-    init_command '/etc/init.d/remote_syslog'
-    provider Chef::Provider::Service::Init::Debian
-    supports :status => true, :start => true, :stop => true, :restart => true
-    action [ :enable, :start ]
+    notifies :restart, "service[remote_syslog]", :delayed
 end
