d66bcf4eb787122c1ff36f591d281a5e0978a551
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2014-02-18 14:24:54 +0000

    Cleanup/add defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default[:remote_syslog][:host] = 'logs.papertrailapp.com'
 default[:remote_syslog][:port] = '514'
-default[:remote_syslog][:log_files] = {}
+default[:remote_syslog][:log_files] = ['/var/log/chef/client.log']
